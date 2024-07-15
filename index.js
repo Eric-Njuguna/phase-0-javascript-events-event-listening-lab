011f5fe206b8b5cb1d0f15668bafd1f7d998f653
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-15 21:58:01 +0300

    Adds an event listener for the button in index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.getElementById(`button`);
+    return input.addEventListener(`click`, () => alert(`I was clicked!`));
 }
+
+addingEventListener();
